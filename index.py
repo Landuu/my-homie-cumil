5201d646b7abb0825fe0cdef3e42066f432ad87f
Bartłomiej Wiercigroch <bw057219@student.ath.edu.pl>
2023-09-05 23:33:57 +0200

    Added .env

--- index.py
+++ index.py
@@ -1,20 +1,24 @@
+import os
 from flask import Flask
 from tweety import Twitter
-  
+from dotenv import load_dotenv
+
 app = Flask(__name__)
 app.debug = True
+load_dotenv()
 
 def filter_retweets(var):
     return hasattr(var, "is_retweet") and not var.is_retweet
 
 @app.route('/')
 def hello_world():
-    return 'Hello World'
+    print(os.getenv("TW_USERNAME"))
+    return 'Hello World! '
 
 @app.route('/test')
 def test():
     tw = Twitter("session")
-    tw.start('email@gmail.com', 'password')
+    tw.start(os.getenv('TW_USERNAME'), os.getenv('TW_PASSWORD'), extra=os.getenv('TW_EXTRA'))
     tweets = tw.get_tweets(username="2Dgirlenjoyer", replies=False)
     
     filtered = filter(filter_retweets, tweets)
